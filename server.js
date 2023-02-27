067e87d21325571ac4fbb03f9e9a24f73d868c81
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-02-27 21:26:11 +0200

    add .env

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Mykola:8TJ4NCVdHEf7rScz@cluster0.zbqgdms.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
+    console.log("Database connection successful");
   })
   .catch((error) => {
     console.log(error);
