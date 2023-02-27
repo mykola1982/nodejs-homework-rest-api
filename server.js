ca65bf9befa52d8881a1a1a080fb45197f2e1c85
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-02-27 20:14:29 +0200

    fixed

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const mongoose = require("mongoose");
+const app = require("./app");
 
 const DB_HOST =
   "mongodb+srv://Mykola:8TJ4NCVdHEf7rScz@cluster0.zbqgdms.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
 
-const app = require("./app");
-
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    console.log("Database connect success ");
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
-    });
+    app.listen(3000);
   })
   .catch((error) => {
     console.log(error.message);
