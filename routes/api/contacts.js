13c69537f48cfedf45f25628b683a9acaf8ebf49
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-02-28 13:06:38 +0200

    add updatefavorite

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,21 +7,34 @@ const {
   getById,
   add,
   updateById,
+  updateStatusContact,
   deleteById,
 } = require("../../controllers");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
-const { addSchema } = require("../../schemas");
+const { schemas } = require("../../models");
 
 router.get("/", getAll);
 
-router.get("/:contactId", getById);
+router.get("/:contactId", isValidId, getById);
 
-router.post("/", validateBody(addSchema), add);
+router.post("/", validateBody(schemas.addSchema), add);
 
-router.delete("/:contactId", deleteById);
+router.delete("/:contactId", isValidId, deleteById);
 
-router.put("/:contactId", validateBody(addSchema), updateById);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.addSchema),
+  updateById
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavorite),
+  updateStatusContact
+);
 
 module.exports = router;
